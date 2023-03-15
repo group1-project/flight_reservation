c0b4bd387c5af251c55b75a458637da2c265c017
Eldoraldi <midhunroy1@gmail.com>
2023-03-15 14:42:29 +0530

    Header and CSS are updated

--- header.php
+++ header.php
@@ -10,6 +10,9 @@
 			rel="stylesheet"
 			href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"
 		/>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Caveat:wght@400;700&family=Homemade+Apple&display=swap" rel="stylesheet">
         <link rel="stylesheet" href="CSS/style.css">
 </head>
 <body>
